e0448ab27b096b5b46cc04754d32926f14866963
IvanDimovSIT <ivan390@abv.bg>
2024-11-16 22:59:18 +0200

    Fixed typo

--- src/services/auth/refresh_token.rs
+++ src/services/auth/refresh_token.rs
@@ -4,13 +4,14 @@ use crate::{
     api::{
         auth::refresh_token::{RefreshTokenInput, RefreshTokenOutput},
         error_response::ErrorResponse,
-    }, app_state::AppState, persistence::user::{find_user_by_id, Model}, security::Claims, util::{create_token_from_user, require_some}
+    },
+    app_state::AppState,
+    persistence::user::{find_user_by_id, Model},
+    security::Claims,
+    util::{create_token_from_user, require_some},
 };
 
-async fn find_user(
-    app_state: &AppState,
-    claims: &Claims,
-) -> Result<Model, ErrorResponse> {
+async fn find_user(app_state: &AppState, claims: &Claims) -> Result<Model, ErrorResponse> {
     let option_find_user = find_user_by_id(app_state.db.as_ref(), &claims.user_id).await?;
 
     let user = require_some(
@@ -26,7 +27,11 @@ pub async fn refresh_token(
     app_state: &AppState,
     input: RefreshTokenInput,
 ) -> Result<RefreshTokenOutput, ErrorResponse> {
-    let claims = require_some(input.claims, || "Claims are empty".to_string(), StatusCode::INSUFFICIENT_STORAGE)?;
+    let claims = require_some(
+        input.claims,
+        || "Claims are empty".to_string(),
+        StatusCode::INTERNAL_SERVER_ERROR,
+    )?;
     let user = find_user(app_state, &claims).await?;
 
     let token = create_token_from_user(&user, app_state)?;
